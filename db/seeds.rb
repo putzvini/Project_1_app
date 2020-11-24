420bcd18162c04136129ab051be760768fed8db1
Vitor Dal Pra <vitordalpra@gmail.com>
2020-11-24 12:10:11 -0300

    seed added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,60 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Creating users'
+
+user1 = User.create!(username: "user1",
+                     email: "user1@user1.com",
+                     password: 123456
+                    )
+
+user2 = User.create!(username: "user2",
+                     email: "user2@user2.com",
+                     password: 123456
+                    )
+
+puts 'Users done'
+
+puts 'adding products'
+    product = Product.create!(
+      name: 'Embroidery Kit For Beginner',
+      details: 'Contains the necessary items you need to start and finish your first embroidery.',
+      price: (50..500).rand(1),
+      category: 'Craft Supplies',
+      user: user1
+      )
+
+    product = Product.create!(
+      name: 'Photo Socks',
+      details: 'Create a truly unique gift for that special person or add a personal touch to your own outfits with our fully customizable socks!',
+      price: (50..500).rand(1),
+      category: 'Clothing',
+      user: user2
+      )
+
+    product = Product.create!(
+      name: 'PRINT flying bumblebee',
+      details: 'This is an art print on paper of my flying bumblebee drawing.',
+      price: (50..500).rand(1),
+      category: 'Art',
+      user: user1
+      )
+
+    product = Product.create!(
+      name: 'Among Us Crewmate Plush Game',
+      details: 'Very Limited Stock, Only Available for a short time!',
+      price: (50..500).rand(1),
+      category: 'Toys',
+      user: user2
+      )
+
+    product = Product.create!(
+      name: 'White Pottery Mug Ceramic Mug',
+      details: "This white pottery mug is great for slow coffee drinkers. It's volume is about 10,5 oz. (300ml)",
+      price: (50..500).rand(1),
+      category: 'Home',
+      user: user1
+      )
+
+puts '2 Users and 5 Products created!'
