b53450d87dc74f07e565fb5f19b168cf206b193b
vinicio oliveira <um.vinicio@gmail.com>
2020-11-25 12:14:09 -0300

    user photo cloundinary

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
+  root to: 'products#index'
   devise_for :users
 
-  devise_scope :user do
-    authenticated :user do
-      root 'pages#home', as: :authenticated_root
-    end
-    unauthenticated do
-      root 'devise/sessions#new', as: :unauthenticated_root
-     end
-  end
+  # devise_scope :user do
+  #   authenticated :user do
+  #     root 'pages#home', as: :authenticated_root
+  #   end
+  #   unauthenticated do
+  #     root 'devise/sessions#new', as: :unauthenticated_root
+  #    end
+  # end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :products do
     resources :purchases, only: [:create]
